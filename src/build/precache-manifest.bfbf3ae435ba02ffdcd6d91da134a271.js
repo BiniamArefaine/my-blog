967546163246fc6a94f365865ca5ec5df3a6c686
Biniam Arefaine <binireyes@gmail.com>
2021-11-05 17:00:12 -0500

    committed

---
+++ src/build/precache-manifest.bfbf3ae435ba02ffdcd6d91da134a271.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc54d8f2b19f905d8be2",
+    "url": "/static/css/main.95ba4f30.chunk.css"
+  },
+  {
+    "revision": "dc54d8f2b19f905d8be2",
+    "url": "/static/js/main.9896f7e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "61c4c899622b805a72bd",
+    "url": "/static/js/2.dffe7372.chunk.js"
+  },
+  {
+    "revision": "fe792e147addcf112f30f87a3488ae92",
+    "url": "/index.html"
+  }
+];
